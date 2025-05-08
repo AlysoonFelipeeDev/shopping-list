5f954afebfd7888066c2e725b565fd2f35e7810c
jean <alysoonfelipe@hotmail.com>
2025-05-07 21:27:38 -0300

    adiciona função para registrar item novo na lista de compras

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ const port = 5000
 
 const shoppingList = [];
 
+app.post("items", (req, res) => {
+    const { name, quantity, type} = req.body;
 
+    if(
+        typeof name !== 'string' || name.trim() === '' ||
+        !Number.isInteger(quantity) || quantity >= 0 ||
+        typeof type !== 'string' || type.trim() === ''
+    ){
+        return res.status(422).send("Todos os dados são obrigatórios!");
+    }
+
+    const addNewItem = { name, quantity, type };
+    const equalItens = shoppingList.some(n => n.name === addNewItem.name);
+    if(equalItens){
+        return res.status(409).send("Já existe esse item!")
+    }
+
+    shoppingList.push(addNewItem);
+    res.status(201).send("Item adicionado");
+})
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
 });
